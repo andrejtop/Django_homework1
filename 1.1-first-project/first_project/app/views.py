27534d9a2019e6dedf40d859e70921884c5d0c15
AndreyToptaev <Bonafide111@yandex.ru>
2023-07-12 11:09:12 +0300

    dz1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import os
+import datetime
 
 
 def home_view(request):
@@ -8,8 +10,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
     }
     
     # context и параметры render менять не нужно
@@ -20,16 +22,21 @@ def home_view(request):
     return render(request, template_name, context)
 
 
-def time_view(request):
+def time_view(request) -> str:
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
     current_time = None
-    msg = f'Текущее время: {current_time}'
+    msg = f"Текущее время: {datetime.datetime.now().time()} <a{reverse('home')}</a>"
     return HttpResponse(msg)
 
 
-def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
-    raise NotImplemented
+def workdir_view(request) -> str:
+    files = []
+    current_dir = os.getcwd()
+    # Получаем список содержимого директории
+    contents = os.listdir(current_dir)
+    for content in contents:
+        files.append(content)
+    all_dir_files = "\n".join(files)
+
+    return HttpResponse(all_dir_files)
