5dfdef09a1f1ea06d6ab5b8d4e607f702214ca8c
AndreyToptaev <Bonafide111@yandex.ru>
2023-08-28 10:22:30 +0300

    course-detail

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -27,8 +27,8 @@ def student_factory():
 @pytest.mark.django_db
 def test_get_first_course(client, course_factory):
     courses = course_factory(_quantity=10)
-    url = reverse("courses-list")
-    response = client.get(url + f"{courses[0].id}/")
+    url = reverse("courses-detail", args=[courses[0].id])
+    response = client.get(url)
     assert response.status_code == 200
     data = response.json()
     assert data.get('id') == courses[0].id
@@ -44,15 +44,26 @@ def test_list_courses(client, course_factory):
     assert len(data) == 10
 
 @pytest.mark.django_db
-def test_filter_courses_by_id_by_name(client, course_factory):
+def test_filter_courses_by_id(client, course_factory):
     course = course_factory(_quantity=10)
-    query_params = {'id': course[5].id, 'name': course[5].name}
+    query_params = {'id': course[5].id}
     url = reverse("courses-list")
     response = client.get(url, params=query_params)
     data = response.json()
     assert response.status_code == 200
     assert len(data) == 10
     assert data[5]['id'] == course[5].id
+
+
+@pytest.mark.django_db
+def test_filter_courses_by_name(client, course_factory):
+    course = course_factory(_quantity=10)
+    query_params = {'name': course[5].name}
+    url = reverse("courses-list")
+    response = client.get(url, params=query_params)
+    data = response.json()
+    assert response.status_code == 200
+    assert len(data) == 10
     assert data[5]['name'] == course[5].name
 
 @pytest.mark.django_db
